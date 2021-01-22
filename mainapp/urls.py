7d577d0abc3b0a183af39a778447ca451c68095d
dikson <ulyanov-649@yandex.ru>
2021-01-22 14:45:41 +0300

    сокоуровневое кэширование контролера def products

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -2,13 +2,15 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
+from django.views.decorators.cache import cache_page
+
 from mainapp import views as mainviews
 
 app_name = 'mainapp'
 
 urlpatterns = [
     path('', mainviews.products, name='index'),
-    path('<int:pk>/', mainviews.products, name='category'),
+    path('<int:pk>/', cache_page(3600)(mainviews.products), name='category'),
     path('<int:pk>/<page>/', mainviews.products, name='page'),
     path('product/<int:pk>/', mainviews.product, name='product')
 
