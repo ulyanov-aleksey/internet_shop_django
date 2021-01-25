0212a251ffc0777753368269351c95282ccb05ce
dikson <ulyanov-649@yandex.ru>
2021-01-25 16:24:40 +0300

    Добавил сигнал для установки is_active для Продукта категории в зависимости от значения в Категории

--- adminapp/views.py
+++ adminapp/views.py
@@ -1,4 +1,7 @@
 from django.contrib.auth.decorators import user_passes_test
+from django.db import connection
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse, reverse_lazy
@@ -251,6 +254,24 @@ class ProductCategoryUpdateView(UpdateView):
         return context
 
 
+# Сигнал для установки is_active для Продукта категории в зависимости от значения в Категории
+def db_profile_by_type(prefix, type, queries):
+    update_queries = list(filter(lambda x: type in x['sql'], queries))
+    print(f'db_profile {type} for {prefix}:')
+    [print(query['sql']) for query in update_queries]
+
+
+@receiver(pre_save, sender=ProductsCategores)
+def product_is_active_update_productcategory_save(sender, instance, **kwargs):
+    if instance.pk:
+        if instance.is_active:
+            instance.product_set.update(is_active=True)
+        else:
+            instance.product_set.update(is_active=False)
+
+        db_profile_by_type(sender, 'UPDATE', connection.queries)
+
+
 # @user_passes_test(lambda u: u.is_superuser)
 # def category_delete(request, pk):
 #     category_item = get_object_or_404(ProductsCategores, pk=pk)
@@ -426,6 +447,7 @@ class ProductUpdateView(UpdateView):
     model = Products
     template_name = 'adminapp/product_update.html'
     form_class = ProductEditForm
+
     # success_url = reverse_lazy('adminapp:product_update')
 
     @method_decorator(user_passes_test(lambda u: u.is_superuser))
