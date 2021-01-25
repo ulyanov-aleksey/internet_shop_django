39483c54d66bb34bbd43b39fa26072a77c4d6f1b
dikson <ulyanov-649@yandex.ru>
2021-01-25 16:53:14 +0300

    Добавление возможности применять скидку к товарам

--- adminapp/forms.py
+++ adminapp/forms.py
@@ -13,9 +13,12 @@ class ShopUserAdminEditForm(ShopUserEditForm):
 
 
 class ProductCategoryEditForm(forms.ModelForm):
+    discount = forms.IntegerField(label='Скидка', required=False, min_value=0, max_value=90, initial=0)
+
     class Meta:
         model = ProductsCategores
-        fields = '__all__'
+        # fields = '__all__'
+        exclude = ()
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
--- adminapp/views.py
+++ adminapp/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.decorators import user_passes_test
 from django.db import connection
+from django.db.models import F
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
 from django.http import HttpResponseRedirect
@@ -252,6 +253,15 @@ class ProductCategoryUpdateView(UpdateView):
         context = super().get_context_data(**kwargs)
         context['title'] = 'категория/редактирование'
         return context
+
+    def form_valid(self, form):
+        if 'discount' in form.cleaned_data:
+            discount = form.cleaned_data['discount']
+            if discount:
+                self.object.product_set.update(price=F('price') * (1 - discount / 100))
+                # db_profile_by_type(self.__class__, 'UPDATE', connection.queries)
+
+        return super().form_valid(form)
 
 
 # Сигнал для установки is_active для Продукта категории в зависимости от значения в Категории
