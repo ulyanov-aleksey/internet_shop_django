859a8b4c72e33b0651a386257f55f3f44a87101a
dikson <ulyanov-649@yandex.ru>
2021-01-27 11:25:33 +0300

    создание теста (на открыте код 200 главной страницы)

--- mainapp/tests.py
+++ mainapp/tests.py
@@ -9,14 +9,34 @@ from django.core.management import call_command
 # Create your tests here.
 
 class TestMainappSmoke(TestCase):
+
     def setUp(self):
-        call_command('flush', '--noinput')
-        call_command('loaddata', 'test_db.json')
+
+        test_category = ProductsCategores.objects.create(name='Тест')  # в пустой бд(для теста) создаем категорию
+        Products.objects.create(                      # создаем продукт в этой категории
+            category=test_category,
+            name='Product_test_1'
+        )
+        Products.objects.create(
+            category=test_category,
+            name='Product_test_2'
+        )
+
         self.client = Client()
 
     def test_mainapp_urls(self):
-        response = self.client.get('')   # какой url тестируем
-        self.assertEqual(response.status_code, 200)
+        response = self.client.get('/')  # какой url тестируем
+        self.assertEqual(response.status_code, 200)  # проверка на код 200(ОК)
+
+# class TestMainappSmoke(TestCase):
+#     def setUp(self):                    # В этом методе указываем какими данными заполнить тестируемую бд
+#         call_command('flush', '--noinput')
+#         call_command('loaddata', 'test_db.json')
+#         self.client = Client()
+#
+#     def test_mainapp_urls(self):
+#         response = self.client.get('')   # какой url тестируем
+#         self.assertEqual(response.status_code, 200)
 
         # response = self.client.get('/contacts/')
         # self.assertEqual(response.status_code, 200)
@@ -35,5 +55,5 @@ class TestMainappSmoke(TestCase):
         #     response = self.client.get(f'/products/product/{product.pk}/')
         #     self.assertEqual(response.status_code, 200)
 
-    def tearDown(self):
+    def tearDown(self):    # функция для завершения теста(очистка временных данных)
         call_command('sqlsequencereset', 'mainapp', 'authapp', 'ordersapp', 'basketapp')
