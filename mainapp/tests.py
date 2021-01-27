c8c06174192362d3f59d20d1cae8e24377152923
dikson <ulyanov-649@yandex.ru>
2021-01-27 22:51:23 +0300

    коментарии к оформлению теста

--- mainapp/tests.py
+++ mainapp/tests.py
@@ -48,33 +48,11 @@ class TestMainappSmoke(TestCase):
         call_command('sqlsequencereset', 'mainapp', 'authapp', 'ordersapp', 'basketapp')
 
 
-# тестирование дублированием бд
+# тестирование дублированием бд(в def setUp создается временная бд для тестирования)
     # class TestMainappSmoke(TestCase):
-    #     def setUp(self):                    # В этом методе указываем какими данными заполнить тестируемую бд
+    #     def setUp(self):
     #         call_command('flush', '--noinput')
     #         call_command('loaddata', 'test_db.json')
     #         self.client = Client()
     #
-    #     def test_mainapp_urls(self):
-    #         response = self.client.get('')   # какой url тестируем
-    #         self.assertEqual(response.status_code, 200)
 
-    # response = self.client.get('/contacts/')
-    # self.assertEqual(response.status_code, 200)
-    #
-    # response = self.client.get('/products/')
-    # self.assertEqual(response.status_code, 200)
-    #
-    # response = self.client.get('/products/category/0/')
-    # self.assertEqual(response.status_code, 200)
-    #
-    # for category in ProductsCategores.objects.all():
-    #     response = self.client.get(f'/products/category/{category.pk}/')
-    #     self.assertEqual(response.status_code, 200)
-    #
-    # for product in Products.objects.all():
-    #     response = self.client.get(f'/products/product/{product.pk}/')
-    #     self.assertEqual(response.status_code, 200)
-
-    # def tearDown(self):  # функция для завершения теста(очистка временных данных)
-    #     call_command('sqlsequencereset', 'mainapp', 'authapp', 'ordersapp', 'basketapp')
